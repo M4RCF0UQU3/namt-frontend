5baa16d32f1fa96a0a65f632fecd60ff1999b988
eemanon <mrklgrnd@yahoo.com>
2018-01-10 11:01:05 +0100

    added image of new tomato

---
+++ src/scenes/Proprio/addGarden/gardenAdded.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+
+class GardenAdded extends React.Component {
+	  render() {
+		return (
+				<div><h2>Bravo.</h2></div>
+		);
+	   }
+}
+export default GardenAdded;
